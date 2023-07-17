9fcdc96092f50d89c153672f56eddc8d6f5a5921
joaquincorimayo <joaquin97corimayo@gmail.com>
2023-07-17 17:17:28 -0300

    Initial commit

---
+++ src/main/java/com/jfecm/openmanagement/SpringOpenManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jfecm.openmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOpenManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOpenManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jfecm/openmanagement/SpringOpenManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfecm.openmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOpenManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
